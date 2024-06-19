fb01c63029885d0eaf801e86d9469d54b7a8eef1
sohamhaldar <sohamhaldar25@gmail.com>
2024-06-19 18:34:19 +0530

    added error handling

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,6 @@
 import { User } from "../models/user.model.js";
+import {ApiError} from "../utils/ApiError.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
 import {
     searchMusics,
     searchAlbums,
